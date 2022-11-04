8ead33e2d0628cc01af2b5d3296946b7c0a34d31
Polina Shpak <uznavenko.polina@mail.ru>
2022-11-04 20:03:07 +0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React, { useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from './store/hooks/redux';
+import { fetchUsers } from './store/reducers/ActionCreaters';
+import { userSlice } from './store/reducers/userSlice';
+
 
 function App() {
+  const {count,users,isLoading,error}=useAppSelector(state=>state.userReducer)
+  const {increment}=userSlice.actions
+  const dispatch=useAppDispatch()
+
+  useEffect(()=>{
+    dispatch(fetchUsers())
+  },[])
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <h1>{count}</h1>
+    {isLoading && <h1>Идет загрузка...</h1>}
+    {error&& <h1>{error}</h1>}
+    {JSON.stringify(users,null,2)}
+    <button onClick={()=>dispatch(increment(10))} >+</button>
+   
     </div>
   );
 }
---
+++ src/store/reducers/ActionCreaters.ts
@@ -0,0 +1,18 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import { IUser } from "../../models/IUser";
+import { AppDispatch } from "../store";
+import {userSlice} from "./userSlice";
+
+export const fetchUsers=()=>async(dispatch:AppDispatch)=>{
+    try{
+        dispatch(userSlice.actions.usersFetching())
+        const response=await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users')
+        dispatch(userSlice.actions.usersFetchingSeccess(response.data))
+
+    }catch(e:any){
+dispatch(userSlice.actions.usersFetchingError(e.message))
+    }
+}
+
+
